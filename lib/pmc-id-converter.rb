4a7bb3557d45cd08077b000cf29e74b2e50a56a2
Ted Pak <powerpak006@gmail.com>
2014-09-14 02:34:29 -0400

    add tool and email params to be nice to eutils

--- lib/pmc-id-converter.rb
+++ lib/pmc-id-converter.rb
@@ -11,6 +11,8 @@ class PMCIDConverter
   EUTILS_SEARCH = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&usehistory=n'
   EUTILS_FETCH = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml'
   EUTILS_INTERVAL = 1.0/3.0
+  EUTILS_TOOL = ['tool', 'mendeley-pmid-updater']
+  EUTILS_EMAIL = ['email', 'theodore.r.pak@gmail.com']
   
   @@last_access = nil
   
@@ -57,7 +59,7 @@ class PMCIDConverter
       term = "#{id}[#{fields[id_type]}]"
       
       uri = URI(EUTILS_SEARCH)
-      ar = URI.decode_www_form(uri.query) << ["term", term]
+      ar = URI.decode_www_form(uri.query) << ["term", term] << EUTILS_TOOL << EUTILS_EMAIL
       uri.query = URI.encode_www_form(ar)
       
       ncbi_access_wait
@@ -69,7 +71,7 @@ class PMCIDConverter
         end
         
         uri = URI(EUTILS_FETCH)
-        ar = URI.decode_www_form(uri.query) << ["id", nk_id.text]
+        ar = URI.decode_www_form(uri.query) << ["id", nk_id.text] << EUTILS_TOOL << EUTILS_EMAIL
         uri.query = URI.encode_www_form(ar)
         
         ncbi_access_wait
@@ -84,6 +86,9 @@ class PMCIDConverter
       end
     end
     
+    # Limit access to EUtils to no more than three requests a second
+    # as required here: http://www.ncbi.nlm.nih.gov/books/NBK25497/
+    # I can find no such documented limit for the PMC ID Converter API.
     def ncbi_access_wait(wait = EUTILS_INTERVAL)
       @@last_access_mutex ||= Mutex.new
       @@last_access_mutex.synchronize {
